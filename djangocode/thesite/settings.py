b2f9cf868812380a75a5d9c148f56f2e3eeed5c2
shabba <sm5648792@gmail.com>
2022-08-25 16:30:05 +0100

    changed

--- djangocode/thesite/settings.py
+++ djangocode/thesite/settings.py
@@ -9,7 +9,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%h01h@)p4hfki3kmehmd^qyr(04e^@_e^!&*&ip^d^=d1=-&gy'
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
