7bf6a2776b49db3deb97630de675c7f1afaefb63
shabba <sm5648792@gmail.com>
2022-09-07 00:00:09 +0100

    testing

--- thesite/celery.py
+++ thesite/celery.py
@@ -14,7 +14,7 @@ app.conf.beat_schedule = {
         "sendit": {
             "task": "stocks.tasks.massStockQuery",
 #            "schedule": 10.0
-            "schedule": crontab(hour=22, minute=59, day_of_week='mon,tue,wed,thu,fri')
+            "schedule": crontab(hour=23, minute=03, day_of_week='mon,tue,wed,thu,fri')
             }
 }
 
